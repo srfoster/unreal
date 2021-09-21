ab05876880d6f65e442d0e88bb46e24a9bcd408c
Lindsey Handley, Ph.D <lindseyhandley@gmail.com>
2021-09-21 09:25:04 -0700

    added http-easy to deps
    added http-easy to deps

--- info.rkt
+++ info.rkt
@@ -1,6 +1,6 @@
 #lang info
 (define collection "unreal")
-(define deps '("base"))
+(define deps '("base" "http-easy"))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
 (define scribblings '(("scribblings/unreal.scrbl" ())))
 (define pkg-desc "Description Here")
